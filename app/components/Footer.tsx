d6ca85d0223773ed1e72182679cc4e759296b69f
Khushi Roy <roykhushi432@gmail.com>
2024-12-13 11:02:35 +0530

    date change

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-8 pt-8 border-t border-green-700 text-center">
-          <p>&copy; 2023 Smart Kheti. All rights reserved.</p>
+          <p>&copy; 2024 Smart Kheti. All rights reserved.</p>
         </div>
       </div>
     </footer>
